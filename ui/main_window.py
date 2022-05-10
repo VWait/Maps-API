c3fb4752e8f5245b20a9ff28f864c1c9ccad2665
alina <Alina.Shakova.45@yandex.ru>
2022-05-10 18:20:14 +0500

    task 9

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QApplication, QComboBox, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QApplication, QComboBox, QCheckBox, QLineEdit, QPushButton
 from PyQt5.QtGui import QPixmap, QKeyEvent
 from PyQt5.QtCore import Qt
 from app_services.map_app_service import MapAppService
@@ -25,6 +25,9 @@ class MainWindow(QWidget):
         self.label2.setText('Место:')
         self.text = QLineEdit(self)
         self.adress = QLabel(self)
+        self.post = QLabel(self)
+        self.post_box = QCheckBox('Почтовый индекс', self)
+        self.post_box.setChecked(False)
         self.map_box.addItems(['map'] + list(set(['sat', 'skl'])))
         self.pushbutton = QPushButton(self)
         self.pushbutton.setText('Искать')
@@ -38,7 +41,9 @@ class MainWindow(QWidget):
         layout.addWidget(self.map_box)
         layout.addWidget(self.label2)
         layout.addWidget(self.text)
+        layout.addWidget(self.post_box)
         layout.addWidget(self.adress)
+        layout.addWidget(self.post)
         layout.addWidget(self.pushbutton)
         layout.addWidget(self.map_label)
         layout.addWidget(self.pushButton)
@@ -53,24 +58,34 @@ class MainWindow(QWidget):
             if response:
                 json_response = response.json()
                 toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-                print(toponym)
+                if self.post_box.isChecked() is True:
+                    post = toponym['metaDataProperty']['GeocoderMetaData']['Address']
+                    if 'postal_code' in post:
+                        self.post.setText('Почтовый индекс: ' + post['postal_code'])
+                    else:
+                        self.post.setText('Почтовый индекс не найден')
+                else:
+                    self.post.setText('')
                 self.adress.setText('Адерс: ' + toponym['metaDataProperty']['GeocoderMetaData']['Address']['formatted'])
                 toponym = toponym["Point"]["pos"]
-                self.request = Request(self.map_box.currentText(), toponym.split()[0], toponym.split()[1], str(toponym.split()[0]) + ',' + str(toponym.split()[1]))
+                self.request = Request(self.map_box.currentText(), toponym.split()[0], toponym.split()[1],
+                                       str(toponym.split()[0]) + ',' + str(toponym.split()[1]))
         else:
-            self.request = Request(self.map_box.currentText(), self.request.get_longitude(), self.request.get_latitude(), self.request.get_sp_top())
+            self.request = Request(self.map_box.currentText(), self.request.get_longitude(),
+                                   self.request.get_latitude(), self.request.get_sp_top())
         self.show_map()
 
     def click1(self):
         self.text.setText('')
         self.adress.setText('')
+        self.post.setText('')
+        self.post_box.setChecked(False)
         self.map_box.setCurrentText('map')
         self.request = Request(self.start[0], self.start[1], self.start[2], self.start[3])
         self.show_map()
 
     def show_map(self):
         map = self.app_service.execute(self.request)
-
         pixmap = QPixmap()
         if self.request.get_l() == 'map' or self.request.get_l() == 'skl':
             pixmap.loadFromData(map, 'PNG')
