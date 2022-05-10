7d5af68a298929fdb6e89309392f02bd181b4f3b
alina <Alina.Shakova.45@yandex.ru>
2022-05-10 17:34:45 +0500

    task 8

--- ui/__init__.py
+++
@@ -1,3 +0,0 @@
-{'metaDataProperty': {'GeocoderMetaData': {'AddressDetails':
-{'Country': {'AdministrativeArea': {'AdministrativeAreaName': 'Тюменская область', 'SubAdministrativeArea':
-{'SubAdministrativeAreaName': 'городской округ Тюмень'}}}}}}}
